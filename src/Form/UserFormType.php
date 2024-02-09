328f79791a9706432bda5279550526cdccd9e9ad
david.didnik <david.didnik@leplusduweb.com>
2024-02-09 12:08:13 +0100

    [feat] : create form for user registration

---
+++ src/Form/UserFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', null, [
+                'required' => true,
+            ])
+            ->add('email', null, [
+                'required' => true,
+            ])
+            ->add('firstname', null, [
+                'required' => true,
+            ])
+            ->add('lastname', null, [
+                'required' => true,
+            ])
+            ->add('password', PasswordType::class, [
+                'required' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
