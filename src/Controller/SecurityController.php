b721d9fbffd1ecb242063360dd9fe8ef79370b7a
david.didnik <david.didnik@leplusduweb.com>
2024-02-09 12:07:53 +0100

    [feat] : add securitycontroller & add register method in it

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Form\UserFormType;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+    }
+
+    #[Route(path: '/register', name: 'app_register')]
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $form = $this->createForm(UserFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On récupère les données du formulaire
+            $registrationForm = $form->getData();
+
+            // On récupère le mot de passe en clair
+            $plainPassword = $registrationForm->getPassword();
+
+            // On hash le mot de passe
+            $hashedPassword = $passwordHasher->hashPassword(
+                $registrationForm,
+                $plainPassword
+            );
+
+            // On remplace le mot de passe en clair par le mot de passe hashé
+            $registrationForm->setPassword($hashedPassword);
+
+            // On sauvegarde l'utilisateur en base de données
+            $entityManager->persist($registrationForm);
+            $entityManager->flush();
+
+            // On redirige l'utilisateur vers la page de connexion
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
