864a5923fb6be53364e2d7163374ec8264e688b9
david.didnik <david.didnik@leplusduweb.com>
2024-02-21 08:56:33 +0100

    [feat] : add start of new tricks logic

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -5,7 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Tricks;
+use App\Form\TricksFormType;
 
 class TricksController extends AbstractController
 {
+    #[Route(path: '/nouveau-trick', name: 'app_tricks_new')]
+    public function new(): Response
+    {
+        $tricks = new Tricks();
+
+        $form = $this->createForm(TricksFormType::class, $tricks);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ... save the tricks
+        }
+
+        return $this->render('tricks/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }
