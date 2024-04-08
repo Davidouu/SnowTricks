0ed0783ee62d3f5af46778459eeddbfdd94eede1
david.didnik <david.didnik@leplusduweb.com>
2024-04-08 13:43:09 +0200

    [fix] : Remove commented out code in User, CategoryRepository, CommentRepository, ImageRepository, TrickRepository, UserRepository, and VideoRepository

--- src/Repository/TrickRepository.php
+++ src/Repository/TrickRepository.php
@@ -34,29 +34,4 @@ class TrickRepository extends ServiceEntityRepository
             false            
         );
     }
-
-//    /**
-//     * @return Trick[] Returns an array of Tricks objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Trick
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
